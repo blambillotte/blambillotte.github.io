61d04314c4fb64a54edae508421346edae087325
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-12-10 19:56:48 -0800

    Added text shadow effect

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -11,14 +11,11 @@
 
   function handleMouseMove(event) {
 
-    var mouseX = event.pageX;
-    var mouseY = event.pageY;
-
-    var percentX = findPercentPosition(viewportWidth, mouseX);
-    var percentY = findPercentPosition(viewportHeight, mouseY);
+    var percentX = findPercentPosition(viewportWidth, event.pageX);
+    var percentY = findPercentPosition(viewportHeight, event.pageY);
 
     rotateDiv($nameDiv, percentX, percentY);
-
+    cssShadow($nameDiv, percentX, percentY);
   }
 
   function findPercentPosition(totalDist, mousePostion) {
@@ -31,10 +28,12 @@
   // Full Y rotation:  transform: rotateY(50deg)
   function getDegRotate(percentX, percentY) {
 
+    var MULTIPLIER = (1/3);
     var degrees = {
-      x: percentX - 50,
-      y: (percentY - 50) * -1
+      x: Math.floor((percentX - 50) * MULTIPLIER),
+      y: Math.floor(((percentY - 50) * -1) * MULTIPLIER)
     }
+    console.log(degrees);
     return degrees;
 
   }
@@ -50,5 +49,32 @@
     div.style.transform = getTransformValue(percentX, percentY);
   }
 
+
+ /****** Text Shadow *****
+  * Example text-shadow: 1px 1px 2px black;
+ */
+
+ function getPxMovement(percentX, percentY) {
+
+   var MULTIPLIER = (1/5);
+   var px = {
+     x: ((percentX - 50) * -1) * MULTIPLIER,
+     y: ((percentY - 50) * -1) * MULTIPLIER
+   }
+   console.log(px);
+   return px;
+
+ }
+
+ function getShadowCss(percentX, percentY, blurPx) {
+
+   var pixles = getPxMovement(percentX, percentY);
+   var COLOR = '#006b5d';
+   return pixles.x + 'px ' + pixles.y + 'px ' + blurPx + 'px ' + COLOR;
+ }
+
+ function cssShadow(div, percentX, percentY) {
+   div.style.textShadow = getShadowCss(percentX, percentY, 2);
+ }
 
 })();
